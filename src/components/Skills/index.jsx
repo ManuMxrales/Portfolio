eaa358d3494aaf181a693d776d138719c3f029e8
Manu Morales <manumxrales@gmail.com>
2023-03-17 13:37:42 -0600

    MVP1 Skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+
 const Skills = () => {
   return (
     <Box
@@ -17,34 +19,142 @@ const Skills = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          justifyContent: "space-between",
+          justifyContent: { xs: "center", md: "space-around" },
           minWidth: "80vw",
           width: { xs: "100%", md: "80%", lg: "90vw" },
           minHeight: "80vh",
+          padding: "3%",
           background: "rgba( 255, 255, 255, 0.2 )",
           backdropFilter: "blur( 2px )",
           borderRadius: "10px",
-          padding: "3%",
         }}
       >
-        <Typography variant="h2" align="center" sx={{ m: 1, color: "black" }}>
-          Fco. Manuel Sanchez
-        </Typography>
-        <Typography variant="body" align="center" sx={{ color: "black" }}>
-          "FrontEnd Developer"
-        </Typography>
-        <Typography variant="subtitle" align="center" sx={{ color: "black" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          temporibus distinctio ducimus sed quo. Ullam corrupti ipsa perferendis
-          iure facere dolores excepturi voluptatum architecto, soluta minima,
-          veritatis reprehenderit modi magni?
-        </Typography>
-        <Typography variant="body" align="center" sx={{ color: "black" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          temporibus distinctio ducimus sed quo. Ullam corrupti ipsa perferendis
-          iure facere dolores excepturi voluptatum architecto, soluta minima,
-          veritatis reprehenderit modi magni?
-        </Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="Typescript"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679081077/DevPortfolio/typescript_taggnt.png"
+            />
+          </Grid>
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="Javascript"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679080932/DevPortfolio/js_pqgdgu.png"
+            />
+          </Grid>
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="NodeJS"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679081166/DevPortfolio/node-js_dl5fce.png"
+            />
+          </Grid>
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="HTML"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679081272/DevPortfolio/html_xreu7e.png"
+            />
+          </Grid>
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="CSS"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679081343/DevPortfolio/css-3_yb1dco.png"
+            />
+          </Grid>
+          <Grid
+            xs={2}
+            sm={4}
+            md={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                width: { xs: "50%", sm: "30%", lg: "50%" },
+                maxWidth: "100%",
+              }}
+              alt="React"
+              src="https://res.cloudinary.com/ddfoio9j1/image/upload/v1679081432/DevPortfolio/physics_xro92x.png"
+            />
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
